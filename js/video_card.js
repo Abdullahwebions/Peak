a338c069e24e2334e8894bc4005455a1e1e59431
Abdullahwebions <131215945+Abdullahwebions@users.noreply.github.com>
2023-08-30 12:33:04 +0300

    Update video_card.js

--- js/video_card.js
+++ js/video_card.js
@@ -52,7 +52,7 @@ export const videoCard = video => {
             <span class="material-symbols-outlined" aria-hidden="true">play_arrow</span>
         </span>
 
-        <a href="${root}peak/videos/video_detail.html?id=${id}" class="state-layer"></a>
+        <a href="${root}Peak/peak/videos/video_detail.html?id=${id}" class="state-layer"></a>
     `;
 
     const /** {NodeList} */ $rippleElems = [$card, $card.querySelector("[data-ripple]")];
